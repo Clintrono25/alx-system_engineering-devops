366883ad8fe89ce6c134280105b5815596bd890e
Clinton Rono <Clinton.Rono@amref.org>
2022-11-01 09:18:36 +0300

    Updates

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # connect to a server Execute a command
 
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config':
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
 }
